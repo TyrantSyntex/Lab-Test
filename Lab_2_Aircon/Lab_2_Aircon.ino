61c4ec4f16c9f437bdd87ced24e8cc53d4966a69
TyrantSyntex <Kaushik.26@ichat.sp.edu.sg>
2026-08-12 14:06:16 +0800

    Refactor temperature and knob handling in Aircon program
    Refactor temperature reading and knob handling logic. Improve display logic based on knob changes.

--- Lab_2_Aircon/Lab_2_Aircon.ino
+++ Lab_2_Aircon/Lab_2_Aircon.ino
@@ -2,20 +2,20 @@
 //2621874
 //DEEE/FT/1B/22
 //Aircon programme
-//Tarun Kumar
-//P2603616
-//DEEE/FT/1B/22
 
 #include <Wire.h>
 #include "RichShieldTM1637.h"
 #include "RichShieldNTC.h"
-#define NTC_PIN A1 //SIG pin of NTC module connect to A1 of IO Shield
+#define NTC_PIN A1
 NTC temper(NTC_PIN);  
 #define LED_BLUE 6
-#define CLK 10//CLK of the TM1637 IC connect to D10 of OPEN-SMART UNO R3
-#define DIO 11//DIO of the TM1637 IC connect to D11 of OPEN-SMART UNO R3
+#define CLK 10
+#define DIO 11
 TM1637 disp(CLK,DIO);
-#define VR_PIN A0   // blue knob/potentiometer
+#define VR_PIN A0
+
+int lastKnobValue = 0;         // remembers the knob's last reading
+unsigned long knobChangedTime = 0;   // remembers WHEN the knob was last turned
 
 void setup()
 {
@@ -26,18 +26,34 @@ void setup()
 
 void loop()
 {
-  float celsius;
-  celsius = temper.getTemperature();
+  float celsius = temper.getTemperature();
+  int knobValue = analogRead(VR_PIN);
+  int threshold = map(knobValue, 0, 1023, 25, 35);
 
-  int threshold = map(analogRead(VR_PIN), 0, 1023, 25, 35);  // knob sets 25-35°C
+  // Check if knob moved since last time
+  int knobDifference = knobValue - lastKnobValue;
+  if (knobDifference < 0) {
+    knobDifference = -knobDifference;   // make it positive, same as abs()
+  }
 
-  displayTemperature((int8_t)celsius, threshold);
-  delay(1000);
+  if (knobDifference > 15) {
+    knobChangedTime = millis();     // record the time it was turned
+    lastKnobValue = knobValue;      // update last known knob position
+  }
+
+  // If it's been less than 2000ms (2 sec) since the knob was turned, show threshold
+  if (millis() - knobChangedTime < 2000) {
+    displayTemperature((int8_t)threshold, threshold);
+  } else {
+    displayTemperature((int8_t)celsius, threshold);
+  }
+
+  delay(200);
 }
 
 void displayTemperature(int8_t temperature, int threshold)
 {
-  int8_t original = temperature;   // save the real value before we chop it up for display
+  int8_t original = temperature;
   int8_t temp[4];
   if(temperature < 0)
   {
@@ -49,19 +65,16 @@ void displayTemperature(int8_t temperature, int threshold)
   temperature %= 100;
   temp[1] = temperature / 10;
   temp[2] = temperature % 10;
-  temp[3] = 12;  // 'C' symbol
+  temp[3] = 12;
   disp.display(temp);
 
   if(original >= threshold)
   {
-    digitalWrite(LED_BLUE, HIGH);   // too hot based on knob setting -> aircon ON
+    digitalWrite(LED_BLUE, HIGH);
   }
   else
   {
-    digitalWrite(LED_BLUE, LOW);    // below threshold -> aircon OFF
+    digitalWrite(LED_BLUE, LOW);
   }
 }
-/*********************************************************************************************************
-The end of file
-*********************************************************************************************************/
 
