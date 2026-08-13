e9bebb3622d41a484360ae82637f7cb59e3d0e43
TyrantSyntex <Kaushik.26@ichat.sp.edu.sg>
2026-08-13 18:26:51 +0800

    Update Burglar.ino

--- Lab_2_Aircon/Burglar.ino
+++ Lab_2_Aircon/Burglar.ino
@@ -1,81 +1,65 @@
 #include <Wire.h>
 #include "RichShieldTM1637.h"
+#include "RichShieldPassiveBuzzer.h"
 
 #define CLK 10
 #define DIO 11
-
-#define LDR A2          // Light sensor pin
-#define BUTTON 8        // Button 1
-#define BUZZER 3        // Buzzer
+#define LDR A2
+#define BUTTON 8
+#define BuzzerPin 3
 
 TM1637 disp(CLK, DIO);
+PassiveBuzzer buz(BuzzerPin);
 
-bool armed = false;
-bool alarm = false;
+bool armed = false, alarm = false;
 int baseLight;
+
+#define NOTE_H1  1046
+#define NOTE_H5  1568
+
+// Alarm pattern: alternating high notes
+const static int alarmNotes[] = {
+  NOTE_H1, NOTE_H5, NOTE_H1, NOTE_H5
+};
+const static int alarmTempo[] = {
+  4, 4, 4, 4
+};
 
 void setup() {
   disp.init();
-
   pinMode(BUTTON, INPUT_PULLUP);
-  pinMode(BUZZER, OUTPUT);
-
-  digitalWrite(BUZZER, LOW);
-
   showOFF();
 }
 
 void loop() {
   checkButton();
-
-  if (armed && !alarm)
-    detectIntruder();
-
-  if (alarm)
-    soundAlarm();
+  if (armed && !alarm) detectIntruder();
+  if (alarm) soundAlarm();
 }
 
 void checkButton() {
   static bool lastState = HIGH;
-
   bool current = digitalRead(BUTTON);
-
   if (lastState == HIGH && current == LOW) {
-
     armed = !armed;
     alarm = false;
-
-    if (armed) {
-      baseLight = analogRead(LDR);
-      showON();
-    }
-    else {
-      digitalWrite(BUZZER, LOW);
-      showOFF();
-    }
-
+    if (armed) { baseLight = analogRead(LDR); showON(); }
+    else { showOFF(); }
     delay(300);
   }
-
   lastState = current;
 }
 
 void detectIntruder() {
-  int light = analogRead(LDR);
-
-  if (abs(light - baseLight) > 80) {
-    alarm = true;
-  }
+  if (abs(analogRead(LDR) - baseLight) > 80) alarm = true;
 }
 
 void soundAlarm() {
-  while (alarm) {
-
-    digitalWrite(BUZZER, HIGH);
-    delay(200);
-
-    digitalWrite(BUZZER, LOW);
-    delay(200);
+  int size = sizeof(alarmNotes) / sizeof(int);
+  for (int i = 0; i < size && alarm; i++) {
+    int noteDuration = 500 / alarmTempo[i];
+    buz.playTone(alarmNotes[i], noteDuration);
+    delay(noteDuration * 0.1);
 
     if (digitalRead(BUTTON) == LOW) {
       alarm = false;
@@ -86,12 +70,5 @@ void soundAlarm() {
   }
 }
 
-void showON() {
-  int8_t data[] = {0, 12, 0, 12};   // Displays "ON" approximately
-  disp.display(data);
-}
-
-void showOFF() {
-  int8_t data[] = {0, 15, 15, 15};  // Simple "OFF" indication
-  disp.display(data);
-}
+void showON()  { int8_t d[] = {0, 12, 0, 12};  disp.display(d); }
+void showOFF() { int8_t d[] = {0, 15, 15, 15}; disp.display(d); }
