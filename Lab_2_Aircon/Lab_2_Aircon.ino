25a9b7f996fd13189225fbbf8b608eb2e27b2119
TyrantSyntex <Kaushik.26@ichat.sp.edu.sg>
2026-08-13 18:33:34 +0800

    Update Lab_2_Aircon.ino

--- Lab_2_Aircon/Lab_2_Aircon.ino
+++ Lab_2_Aircon/Lab_2_Aircon.ino
@@ -13,9 +13,8 @@ NTC temper(NTC_PIN);
 #define DIO 11
 TM1637 disp(CLK,DIO);
 #define VR_PIN A0
-
-int lastKnobValue = 0;         // remembers the knob's last reading
-unsigned long knobChangedTime = 0;   // remembers WHEN the knob was last turned
+int lastKnobValue = 0;
+unsigned long knobChangedTime = 0;
 
 void setup()
 {
@@ -30,18 +29,16 @@ void loop()
   int knobValue = analogRead(VR_PIN);
   int threshold = map(knobValue, 0, 1023, 25, 35);
 
-  // Check if knob moved since last time
   int knobDifference = knobValue - lastKnobValue;
   if (knobDifference < 0) {
-    knobDifference = -knobDifference;   // make it positive, same as abs()
+    knobDifference = -knobDifference;
   }
 
   if (knobDifference > 15) {
-    knobChangedTime = millis();     // record the time it was turned
-    lastKnobValue = knobValue;      // update last known knob position
+    knobChangedTime = millis();
+    lastKnobValue = knobValue;
   }
 
-  // If it's been less than 2000ms (2 sec) since the knob was turned, show threshold
   if (millis() - knobChangedTime < 2000) {
     displayTemperature((int8_t)threshold, threshold);
   } else {
@@ -55,26 +52,25 @@ void displayTemperature(int8_t temperature, int threshold)
 {
   int8_t original = temperature;
   int8_t temp[4];
-  if(temperature < 0)
-  {
-    temp[0] = INDEX_NEGATIVE_SIGN;
-    temperature = abs(temperature);
+  bool negative = (temperature < 0);
+  if (negative) temperature = abs(temperature);
+
+  int digits[3] = { temperature / 100, (temperature / 10) % 10, temperature % 10 };
+
+  // Loop through the digits array to fill the display array
+  for (int i = 0; i < 3; i++) {
+    temp[i] = digits[i];
   }
-  else if(temperature < 100) temp[0] = INDEX_BLANK;
-  else temp[0] = temperature/100;
-  temperature %= 100;
-  temp[1] = temperature / 10;
-  temp[2] = temperature % 10;
-  temp[3] = 12;
+
+  if (negative) temp[0] = INDEX_NEGATIVE_SIGN;
+  else if (temperature < 100) temp[0] = INDEX_BLANK;
+
+  temp[3] = 12;  // 'C' symbol
   disp.display(temp);
 
-  if(original >= threshold)
-  {
+  if (original >= threshold) {
     digitalWrite(LED_BLUE, HIGH);
-  }
-  else
-  {
+  } else {
     digitalWrite(LED_BLUE, LOW);
   }
 }
-
